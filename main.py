3a192d0b409e4dc809bde7fe24150d5e0b003136
jameskrueger00 <79768190+jameskrueger00@users.noreply.github.com>
2023-10-06 13:47:49 -0600

    feature: adds examples to schema and strategy flag to CLI

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ import openai
 import os
 
 from query import select_from_table
-from schema import get_schema
+from schema import get_schema, get_schema_with_examples
 from db import create_connection
 from dotenv import load_dotenv
 
 DATABASE = "./pythonsqlite.db"
 
 
-def main(conn, question):
-    load_dotenv()
-
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    print(f"Question: {question}")
-
-    prompt = f"""
+def get_prompt_string(question, strategy):
+    if strategy == 1:
+        return f"""
     
     Given the following SQL Schema:{get_schema()}
     Write a SQL query to answer this question: {question}
     
     """
+    elif strategy == 2:
+        return f"""
+        
+        Given the following SQL Schema:{get_schema_with_examples()}
+        Write a SQL query to answer this question: {question}
+
+        """
+
+
+def main(conn, question, strategy):
+    load_dotenv()
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    print(f"Question: {question}")
+
+    prompt = get_prompt_string(question, strategy)
 
     response = openai.Completion.create(
         model="text-davinci-003",
@@ -40,7 +52,8 @@ def main(conn, question):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", type=str, default="natural language query")
+    parser.add_argument("--strategy", type=int, default=1)
     args = parser.parse_args()
     conn = create_connection(DATABASE)
 
-    main(conn, question=args.query)
+    main(conn, question=args.query, strategy=args.strategy)
--- query.py
+++ query.py
@@ -27,6 +27,30 @@ def select_from_table(conn, query):
         print(row)
 
 
+def select_example(conn, table):
+    # Fetch column names
+    cur = conn.cursor()
+    cur.execute(f"PRAGMA table_info({table})")
+    columns = [column[1] for column in cur.fetchall()]
+
+    columns_formatted = ', '.join(columns)
+
+    # Fetch rows
+    query = f"SELECT * FROM {table} LIMIT 3"
+    cur.execute(query)
+    rows = cur.fetchall()
+    rows_formatted = '\n'.join([', '.join(map(str, r)) for r in rows])
+
+    return f"""
+/*    
+3 Example Rows:
+SELECT * FROM {table} LIMIT 3;
+{columns_formatted}
+{rows_formatted}
+*/
+"""
+
+
 if __name__ == "__main__":
     database = "./pythonsqlite.db"
     conn = create_connection(database)
